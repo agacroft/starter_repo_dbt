9cd02beca100f04baab5a137b657638c3a37153b
Agnieszka <agus.kaczmarczyk@gmail.com>
2023-10-12 14:48:21 +0200

    staging files

---
+++ models/staging/staging_rates.sql
@@ -0,0 +1,11 @@
+with extracted_rates as (
+    select 
+        ((extracted_data -> 'date')::VARCHAR)::DATE as timestamp,
+        (extracted_data -> 'base')::VARCHAR as base_currency,
+        ((extracted_data -> 'rates' -> 'GBP')::VARCHAR)::FLOAT as gbp,
+        ((extracted_data -> 'rates' -> 'USD')::VARCHAR)::FLOAT as usd,
+        ((extracted_data -> 'rates' -> 'INR')::VARCHAR)::FLOAT as inr
+    from {{source('staging'), 'rates_raw'}}
+) 
+select * 
+from extracted_rates
