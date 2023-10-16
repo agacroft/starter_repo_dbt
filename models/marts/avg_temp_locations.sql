fe5c186750d61a82677dccfab906f96232822ba4
Agnieszka <agus.kaczmarczyk@gmail.com>
2023-10-16 16:19:39 +0200

    update

---
+++ models/marts/avg_temp_locations.sql
@@ -0,0 +1,20 @@
+WITH weekday_name AS (
+    SELECT *
+    FROM {{ref('prep_temp')}}
+),
+weekday_avg  AS (
+    SELECT  city,
+            country,
+            date,
+            lat,
+            lon,
+            region,
+            AVG(maxtemp_c) AS avg_max_temp, 
+            AVG(mintemp_c) AS avg_min_temp, 
+            AVG(avgtemp_c) AS avg_temp
+    FROM weekday_name
+    GROUP BY city, country, date, lat, lon, region
+    ORDER BY day_num
+)
+SELECT * 
+FROM weekday_avg
---
+++ models/marts/week_avg_temp.sql
@@ -0,0 +1,16 @@
+WITH weekday_name AS (
+    SELECT *
+    FROM {{ref('prep_temp')}}
+),
+weekday_avg  AS (
+    SELECT  --day_num,
+            date_part('week', date) AS day_num as week_number,
+            AVG(maxtemp_c) AS avg_max_temp, 
+            AVG(mintemp_c) AS avg_min_temp, 
+            AVG(avgtemp_c) AS avg_temp
+    FROM weekday_name
+    GROUP BY date_part('week', date) AS day_num
+    ORDER BY day_num
+)
+SELECT * 
+FROM weekday_avg
